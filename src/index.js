1d545fc98e769e8083dfae7b1acf4986b6b70145
SeaHawks <tanvirrezaanik@gmail.com>
2022-07-18 01:56:28 +0600

    project initialized with tailwind css, redux and navbar

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import reducer, {initialState} from "./states/reducer";
+import {StateProvider} from './states/StateProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <StateProvider initialState={initialState} reducer={reducer}>
+          <App/>
+      </StateProvider>
   </React.StrictMode>
 );
 
---
+++ src/layouts/navbar.js
@@ -0,0 +1,67 @@
+import React, {useEffect, useRef, useState} from "react";
+import '../styles/navbar.css'
+import {AiOutlineDown, AiOutlineMenu} from "react-icons/ai";
+import image from '../assets/pp.jpg'
+
+const Navbar = ({user}) => {
+    const [toggleMenu, setToggleMenu] = useState(false)
+    const [screenWidth, setScreenWidth] = useState(window.innerWidth)
+    const toggleNav = () => {
+        setToggleMenu(!toggleMenu)
+    }
+    const [modal, setModal] = useState(false);
+    const modalRef = useRef();
+
+    useEffect(() => {
+        const changeWidth = () => {
+            setScreenWidth(window.innerWidth);
+        }
+        window.addEventListener('resize', changeWidth)
+        return () => {
+            window.removeEventListener('resize', changeWidth)
+        }
+    }, [])
+
+    useEffect(() => {
+        document.addEventListener('mousedown', (e) => {
+            if (!modalRef.current.contains(e.target)) {
+                setModal(false)
+            }
+        })
+    }, []);
+
+    return (
+        <nav ref={modalRef}>
+            <ul className="list">
+                <li className='logo'>#DG-Projektportal</li>
+                <li className='time'>Firmenprojeckte | 22.04a</li>
+                {
+                    (toggleMenu || screenWidth > 500) && (
+                        <>
+                            <li className="items">Dashboard</li>
+                            <li className="items">New Creation</li>
+                            <li className="items">Inventory</li>
+                            <li className="items">Bestant</li>
+                            <li className="items">Evaluation</li>
+                            <li className="items">Instruction</li>
+                            <li className='userInfo cursor-pointer'>
+                                <img src={image} alt='image'/>
+                                <div>
+                                    <p onClick={() => setModal(!modal)}>Username</p>
+                                    <p onClick={() => setModal(!modal)} className='text-xs'>Id: 132255214</p>
+                                </div>
+                                <p className='cursor-pointer m-1'><AiOutlineDown onClick={() => setModal(!modal)}/></p>
+                            </li>
+                        </>
+                    )
+                }
+            </ul>
+            <p className={modal ? 'modal-logout' : 'hidden'}>
+                <button> Log Out</button>
+            </p>
+            <button onClick={toggleNav} className="btn"><AiOutlineMenu/></button>
+        </nav>
+    )
+}
+
+export default Navbar
