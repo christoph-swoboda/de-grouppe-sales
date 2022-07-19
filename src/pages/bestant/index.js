db486b2a3250476ce7dbaea695b73d8629c1d4f5
SeaHawks <tanvirrezaanik@gmail.com>
2022-07-19 10:15:17 +0600

    bestant page top section made responsive

--- src/pages/bestant/index.js
+++ src/pages/bestant/index.js
@@ -7,30 +7,28 @@ const Bestant = () => {
     return (
         <div className='dashboardContainer'>
             <h2 className='text-left text-xl pt-5 pb-5'>Bestant</h2>
-            <div className='bg-white p-8 flex'>
-
-                <div className='flex justify-start w-6/12'>
-                    <input className='mr-5 search' type='search' placeholder='sueche..'
-                           onChange={(e) => setSearch(e.target.value)}
-                    />
+            <div className='bg-white p-8 lg:flex sm:block'>
+                <input className='mr-5 search' type='search' placeholder='sueche..'
+                       onChange={(e) => setSearch(e.target.value)}
+                />
+                <div className='flex justify-center m-1'>
                     <RiFileExcel2Fill className='mr-1' size='25px' color={'#388E3C'}/>
-                    <span className='mr-5 text-grey'>Excel Export</span>
+                    <span className='mr-1 mb-1 text-grey '>Excel Export</span>
+                </div>
+                <div className='flex justify-center m-1'>
                     <VscFilePdf className='mr-1' size='25px' color={'#DB2955'}/>
-                    <span className='mr-3 text-grey'>PDF Export</span>
+                    <span className='mr-1 mb-2 text-grey'>PDF Export</span>
                 </div>
 
-                <div className='flex  justify-end w-6/12'>
-                    <p className='text-sm text-grey'>1 bis 10 von 596 Eintragen</p>
-                    <h2 className='text-sm text-grey ml-10'>
-                        Eintrage anzigen: <span>
+                <p className='text-sm text-grey ml-auto mt-2'>1 bis 10 von 596 Eintragen</p>
+                <h2 className='text-sm text-grey ml-6 mt-2 ml-10'>
+                    Eintrage anzigen: <span>
                         <select className='bg-transparent text-mainBlue'>
                             <option value={50}>50</option>
                             <option value={100}>100</option>
                         </select>
                     </span>
-                    </h2>
-                </div>
-
+                </h2>
             </div>
         </div>
     )
