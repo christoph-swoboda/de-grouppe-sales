38b7aa5fc3a8788322d1209f8e5f66352b9bd0aa
SeaHawks <tanvirrezaanik@gmail.com>
2022-09-28 06:05:50 +0600

    design fix for substeps and bestands

--- src/pages/bestant/index.js
+++ src/pages/bestant/index.js
@@ -100,76 +100,70 @@ const Bestant = () => {
             />
 
             <div className='lg:flex justify-between my-3 rounded-lg sm:block'>
-                <div className='bg-white px-2 m-1 pb-10 lg:min-w-max lg:ml-0 rounded-lg h-fit'>
-                    {
-                        loading ?
-                            <div style={{height: '80vh'}}>
-                                <div className='mt-24'>
-                                    <ScaleLoader size={110}/>
+                {
+                    loading ?
+                        <div style={{height: '30vh'}} className='centerItemsAbsolute bg-white w-11/12'>
+                            <div className='mt-24'>
+                                <ScaleLoader size={110}/>
+                            </div>
+                        </div>
+                        :
+                        <>
+                            <div className='bg-white px-2 m-1 pb-10 lg:min-w-max lg:ml-0 rounded-lg h-fit'>
+                                {
+                                    milestoneTabs.map(tab => (
+                                        <MilestoneTabs
+                                            key={tab.milestoneID}
+                                            id={tab.milestoneID}
+                                            label={tab.milestoneLabel}
+                                            loading={stepsLoading}
+                                            done={tab.milestoneDone}
+                                            lastIndex={lastIndex}
+                                            lastDoneIndex={lastDoneIndex}
+                                        />
+                                    ))
+                                }
+                            </div>
+
+                            <div className='lg:w-2/4 2xl:w-2/4 lg:ml-0 h-fit text-left'>
+                                <div className='bg-white p-5 m-2 pb-10'>
+                                    <h2 className='text-2xl text-center text-bold mb-2 '>Platzhalter</h2>
+                                    {/*<hr/>*/}
+                                    {/*<br/>*/}
+                                    <h2 className='text-xl absolute'>{milestoneTabs[Number(currentMilestone)]?.milestoneLabel.substring(3)}</h2>
+                                    <SubSteps
+                                        data={subSteps}
+                                        loading={stepsLoading}
+                                        lastDoneIndex={lastDoneIndex}
+                                        currentSubStep={currentSubStep}
+                                        options={options}
+                                        grid={grid}
+                                    />
+                                </div>
+
+                                <div
+                                    hidden={stepsLoading || Number(currentMilestone) < Number(lastDoneIndex) + 1 || nextStep.length === 0}
+                                    className='my-4 bg-white p-5 m-2 pb-10'
+                                >
+                                    <h2 className='text-2xl'>{milestoneTabs[Number(currentMilestone) + 1]?.milestoneLabel.substring(3)}(Bevorstehende)</h2>
+                                    <SubSteps
+                                        data={nextStep}
+                                        loading={stepsLoading}
+                                        lastDoneIndex={lastDoneIndex}
+                                        currentSubStep={currentSubStep}
+                                        next
+                                        options={options}
+                                        grid={grid}
+                                    />
                                 </div>
                             </div>
-                            :
-                            milestoneTabs.map(tab => (
-                                <MilestoneTabs
-                                    key={tab.milestoneID}
-                                    id={tab.milestoneID}
-                                    label={tab.milestoneLabel}
-                                    loading={stepsLoading}
-                                    done={tab.milestoneDone}
-                                    lastIndex={lastIndex}
-                                    lastDoneIndex={lastDoneIndex}
-                                />
-                            ))
-                    }
-                </div>
-
-                <div className='lg:w-2/4 2xl:w-2/4 lg:ml-0 h-fit text-left'>
-                    <div className='bg-white p-5 m-2 pb-10'>
-                        <h2 className='text-2xl text-center text-bold mb-2 '>Platzhalter</h2>
-                        {/*<hr/>*/}
-                        {/*<br/>*/}
-                        <h2 className='text-xl absolute'>{milestoneTabs[Number(currentMilestone)]?.milestoneLabel.substring(3)}</h2>
-                        <SubSteps
-                            data={subSteps}
-                            loading={stepsLoading}
-                            lastDoneIndex={lastDoneIndex}
-                            currentSubStep={currentSubStep}
-                            options={options}
-                            grid={grid}
-                        />
-                    </div>
-
-                    <div
-                        hidden={stepsLoading || Number(currentMilestone) < Number(lastDoneIndex) + 1 || nextStep.length === 0}
-                        className='my-4 bg-white p-5 m-2 pb-10'
-                    >
-                        <h2 className='text-2xl'>{milestoneTabs[Number(currentMilestone) + 1]?.milestoneLabel.substring(3)}(Bevorstehende)</h2>
-                        <SubSteps
-                            data={nextStep}
-                            loading={stepsLoading}
-                            lastDoneIndex={lastDoneIndex}
-                            currentSubStep={currentSubStep}
-                            next
-                            options={options}
-                            grid={grid}
-                        />
-                    </div>
-                </div>
 
 
-                <div className='bg-white px-3 2xl:w-2/4 pb-10 xl:w-1/3 xl:ml-0 rounded-lg h-fit'>
-                    {
-                        loading ?
-                            <div style={{height: '30vh'}}>
-                                <div className='mt-24'>
-                                    <ScaleLoader size={110}/>
-                                </div>
+                            <div className='bg-white px-3 2xl:w-2/4 pb-10 xl:w-1/3 xl:ml-0 rounded-lg h-fit'>
+                                <Status company={param.id.replaceAll('_', ' ')} notes={notes}/>
                             </div>
-                            :
-                            <Status company={param.id.replaceAll('_', ' ')} notes={notes}/>
-                    }
-                </div>
-
+                        </>
+                }
             </div>
 
             <Modal toggle={toggleCompanyInfoModal}
